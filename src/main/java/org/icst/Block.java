205077a0ef07e74caa9cccdeec460e51edd0f253
Houston613 <mr.antonchervak@gmail.com>
2023-04-07 23:10:10 +0300

    #1 "Block" class created with sha-256 algorithm

---
+++ src/main/java/org/icst/Block.java
@@ -0,0 +1,86 @@
+package org.icst;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Block {
+
+    private long index;
+    private String prevHash;
+    private String hash;
+    private String data;
+    private long nonce;
+
+    public Block(long index, String prevHash, String hash, String data, long nonce) {
+        this.index = index;
+        this.prevHash = prevHash;
+        this.hash = hash;
+        this.data = data;
+        this.nonce = nonce;
+    }
+
+    private String getCurrentState() {
+
+        return getIndex() + getPrevHash() + getData() + getNonce();
+
+    }
+
+    public String hashCalculate() {
+        MessageDigest messageDigest;
+        String resultHash;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(getCurrentState().getBytes());
+
+            resultHash = IntStream.range(0, hash.length)
+                    .mapToObj(i -> String.format("%02x", hash[i]))
+                    .collect(Collectors.joining());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return resultHash;
+    }
+
+
+    public long getIndex() {
+        return index;
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public String getPrevHash() {
+        return prevHash;
+    }
+
+    public void setPrevHash(String prevHash) {
+        this.prevHash = prevHash;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public long getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(long nonce) {
+        this.nonce = nonce;
+    }
+}
